272552ade377d228f3dcf5081a2fb30ad03eb248
Isaac Idibia <idibiaisaac3@gmail.com>
2022-08-29 18:58:20 +0100

    edited subheadings

--- src/components/UI/Container.js
+++ src/components/UI/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = (props) => {
   return (
-      <section className='desktop:max-w-screen-xl laptop:max-w-5xl tablet:max-w-3xl m-auto phone:max-w-[450px] tablet:p-0 phone:p-1.5'>
+      <section className='desktop:max-w-screen-xl laptop:max-w-5xl tablet:max-w-3xl m-auto phone:max-w-[450px] tablet:p-0 phone:p-3'>
         {props.children}
       </section>
   );
