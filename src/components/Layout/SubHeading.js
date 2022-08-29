14243da36d032e9d8e19d4d597aaed68d8e88254
Isaac Idibia <idibiaisaac3@gmail.com>
2022-08-29 20:13:43 +0100

    responsiveness for smaller screen

--- src/components/Layout/SubHeading.js
+++ src/components/Layout/SubHeading.js
@@ -7,8 +7,8 @@ const SubHeading = (props) => {
   return (
     <Container>
       <div className="font-bold bg-yellow-ortho py-4 tablet:px-10 phone:px-3 rounded-lg mt-10 flex justify-between items-center">
-        <p className="tablet:text-2xl phone:text-base">{props.pageTitle}</p>
-        <p className="tablet:text-lg phone:text-sm text-light-black">
+        <p className="tablet:text-2xl phone:text-sm">{props.pageTitle}</p>
+        <p className="tablet:text-lg phone:text-xs text-light-black">
           <a className="tablet:mr-7 phone:mr-2.5 text-black" href="#home">{props.leftIcon}</a>
           {props.date}
           <a className="tablet:ml-7 phone:ml-2.5" href="#home">{props.rightIcon}</a>
